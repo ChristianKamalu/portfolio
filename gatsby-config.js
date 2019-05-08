35eaa7b6cb477f9ebd42aff475b80aa7f3bf0f26
ChristianKamalu <ckamalu98@gmail.com>
2019-05-08 09:24:49 -0600

    add palm tree

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/soccerball.png', // This path is relative to the root of the site.
+        icon: 'src/images/palm_tree.jpg', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
