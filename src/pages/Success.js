7c4897bb7eafc88f3b3032ace2b20d3116399a04
ChristianKamalu <ckamalu98@gmail.com>
2019-05-10 16:24:45 -0600

    make success page

---
+++ src/pages/Success.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const NotFoundPage = () => (
+  <Layout>
+    <h1>Success!!</h1>
+    <p>Message sent.</p>
+  </Layout>
+)
+
+export default NotFoundPage
