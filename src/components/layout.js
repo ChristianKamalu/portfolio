0349cad980a2fad2a186456802b97cc8bc935eca
ChristianKamalu <ckamalu98@gmail.com>
2022-04-19 14:15:28 -0600

    update portfolio

--- src/components/layout.js
+++ src/components/layout.js
@@ -41,8 +41,8 @@ const Layout = ({ children, location }) => {
           <Helmet
             title={data.site.siteMetadata.title}
             meta={[
-              { name: 'description', content: 'Sample' },
-              { name: 'keywords', content: 'sample, something' },
+              { name: 'description', content: 'Full Stack Web Developer' },
+              { name: 'keywords', content: 'coding, web, developer, engineer, website, programmer' },
             ]}
           >
             <html lang="en" />
